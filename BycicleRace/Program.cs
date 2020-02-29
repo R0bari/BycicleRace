9c8c0090a7f6d925bc3be1944e0104f29c4ebaa3
Eugene Sukharev <r0bari@yandex.ru>
2020-02-29 03:10:51 +0300

    Initial commit

---
+++ BycicleRace/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BycicleRace
+{
+    class Program
+    {
+        static double CalculateMeetingPoint(double S, double A, double B)
+        {
+            return A / (A + B) * S;
+        }
+        static void Main()
+        {
+            Console.WriteLine("Введите расстояние между городами:");
+            if (!double.TryParse(Console.ReadLine(), out double S))
+            {
+                Console.WriteLine("Некорректный ввод.");
+                return;
+            }
+
+            Console.WriteLine("Введите скорость первого велосипедиста:");
+            if (!double.TryParse(Console.ReadLine(), out double A))
+            {
+                Console.WriteLine("Некорректный ввод.");
+                return;
+            }
+
+            Console.WriteLine("Введите скорость второго велосипедиста:");
+            if (!double.TryParse(Console.ReadLine(), out double B))
+            {
+                Console.WriteLine("Некорректный ввод.");
+                return;
+            }
+
+            Console.WriteLine("Место встречи: " + CalculateMeetingPoint(S, A, B).ToString("0.0000000") + "-й километр.");
+        }
+    }
+}
